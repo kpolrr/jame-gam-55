f5cd63fdb9b9960d674d0a300e08b6e0d26ff0a0
AmKiwi <superkiwi156@gmail.com>
2025-11-18 20:47:03 +1100

    basically nothing

---
+++ Jame Gam 55/Assets/PlayerController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+
+    void Update()
+    {
+        
+    }
+}
